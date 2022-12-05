5da69fa8a81eff9be40f2d9a0433a8d5f61b848f
Sticky Piston <mr.sticky.piston@gmail.com>
2022-12-05 17:04:21 +0100

    Fixed a bug downloading the files incorrect

--- Main/PyInstaller/typegreek_updater.py
+++ Main/PyInstaller/typegreek_updater.py
@@ -3,8 +3,8 @@ import os
 
 typegreekFolder = os.path.expanduser('~\.typegreek')
 
-url_EN = 'https://github.com/StickyPiston-development/TypeGreek-Windows-Installer/blob/installer/Main/typegreek_EN.exe'
-url_NL = 'https://github.com/StickyPiston-development/TypeGreek-Windows-Installer/blob/installer/Main/typegreek_NL.exe'
+url_EN = 'https://github.com/StickyPiston-development/TypeGreek-Windows-Installer/blob/installer/Main/typegreek_EN.exe?raw=True'
+url_NL = 'https://github.com/StickyPiston-development/TypeGreek-Windows-Installer/blob/installer/Main/typegreek_NL.exe?raw=True'
 
 r_EN = requests.get(url_EN, allow_redirects=True)
 r_NL = requests.get(url_NL, allow_redirects=True)
