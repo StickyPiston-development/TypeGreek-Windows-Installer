84d044cb311750c5c3d72ef2444ac62238e5aec6
Sticky Piston <mr.sticky.piston@gmail.com>
2022-12-11 12:41:02 +0100

    Recoded updater

--- Main/PyInstaller/typegreek_updater.py
+++ Main/PyInstaller/typegreek_updater.py
@@ -1,34 +1,34 @@
 # usr/bin/python3
-import urllib.request, os
+import urllib.request
 
 versionurl = 'https://raw.githubusercontent.com/StickyPiston-development/typegreek-lib/main/version.dat'
 url_EN = 'https://github.com/StickyPiston-development/tgw-lib/blob/main/typegreek_EN.exe?raw=true'
 url_NL = 'https://github.com/StickyPiston-development/tgw-lib/blob/main/typegreek_NL.exe?raw=true'
 
 try:
-	version = open('version.dat', 'r').readline()
-except FileNotFoundError:
-	version = open('version.dat',"w+").write("unknown")
-	version = open('version.dat', 'r').readline()
-
-uf = urllib.request.urlopen(versionurl)
-UpdateVersion = str(uf.read().decode("utf-8"))
-
-if version == UpdateVersion:
-	print("The program is up to date on version " + version)
-else:
-	print("The program needs to be updated.\n\nCurrent version: " + version + "\nUp to date version: " + UpdateVersion)
-
-	typegreekFolder = os.path.expanduser('~\.typegreek')
-
-	EN = urllib.request.urlopen(url_EN)
-	NL = urllib.request.urlopen(url_EN)
-
 	try:
-		open(typegreekFolder + '\\typegreek_EN.exe', 'wb').write(EN.read())
-		open(typegreekFolder + '\\typegreek_NL.exe', 'wb').write(NL.read())
-	except Exception:
-		print("Exception ocurred, trying again later.")
+		version = open('version.dat', 'r').readline()
+	except FileNotFoundError:
+		version = open('version.dat',"w+").write("unknown")
+		version = open('version.dat', 'r').readline()
+
+	uf = urllib.request.urlopen(versionurl)
+	UpdateVersion = str(uf.read().decode("utf-8"))
+
+	if version == UpdateVersion:
+		print("The program is up to date on version " + version)
 	else:
+		print("The program needs to be updated.\n\nCurrent version: " + version + "\nUp to date version: " + UpdateVersion)
+
+		EN = urllib.request.urlopen(url_EN)
+		NL = urllib.request.urlopen(url_EN)
+
+		open('typegreek_EN.exe', 'wb').write(EN.read())
+		open('typegreek_NL.exe', 'wb').write(NL.read())
+		
 		print("Updated the program.")
-		version = open('version.dat',"w+").write(UpdateVersion)
+		
+except Exception:
+	print("An error ocurred. trying again later.")
+else:
+	version = open('version.dat',"w+").write(UpdateVersion)
