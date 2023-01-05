ed34e94f1c003a2b5bc0d86cdc542a2aacd5a39e
Sticky Piston <mr.sticky.piston@gmail.com>
2023-01-05 14:52:16 +0100

    Added confirmation message

--- Main/PyInstaller/install_addon.py
+++ Main/PyInstaller/install_addon.py
@@ -1,4 +1,4 @@
-import sys,os,shutil
+import sys,os,shutil,ctypes
 
 if getattr(sys, 'frozen', False):
     application_path = os.path.dirname(sys.executable)
@@ -22,5 +22,6 @@ if len(sys.argv) > 1:
     addonfile = sys.argv[1]
     print(addonfile)
     shutil.copy(addonfile, addon_folder)
+    ctypes.windll.user32.MessageBoxW(0, "succesfully installed " + os.path.split(addonfile)[1], "TypeGreek Addon installer", 1)
 else:
     print("\nError: args may not be empty")
